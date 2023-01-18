b266846c22dd0ca3329804f6af5a4580be33f01e
Moise-code <moisewowe@gmail.com>
2023-01-18 01:26:42 -0800

    adding the function call to add update the UI

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const startGameButton = document.querySelector('#start-game-btn');
+const addNext = document.querySelector('.add');
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
@@ -14,8 +15,18 @@ const getPlayerChoice = () =>{
     selection !== SCISSORS
   ) {
     alert(`Invalid choice sir, therefore we just picked ${DEFAULT_USER_CHOICE} for you`)
-  }
+    addNext.innerHTML += `<p>The choice that a user chose is ${DEFAULT_USER_CHOICE}</p>`
+    console.log(DEFAULT_USER_CHOICE);
+    return DEFAULT_USER_CHOICE;
+  } else if(selection === ROCK ||
+            selection === PAPER || 
+            selection === SCISSORS
+    ) {
+      addNext.innerHTML += `<p>The choice that a user chose is ${selection}</p>`
+      console.log(selection);
+      return selection;
+    }
 }
 
 
-startGameButton.addEventListener('click', )
+startGameButton.addEventListener('click', getPlayerChoice)
