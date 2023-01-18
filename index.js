5343f199975a8af10a7098f5bc3be38105635d02
Moise-code <moisewowe@gmail.com>
2023-01-18 01:12:05 -0800

    adding the default user choice in the scope

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const startGameButton = document.querySelector('#start-game-btn');
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK
 
 const getPlayerChoice = () =>{
-  const selection = prompt(`choose between ${ROCK}, ${PAPER} or ${SCISSORS}, what do you choose?`, '');
+  const selection = prompt(`choose between ${ROCK}, ${PAPER} or ${SCISSORS}, what do you choose?`, '').toUpperCase() ;
 
   if(
     selection !== ROCK &&
     selection !== PAPER &&
     selection !== SCISSORS
   ) {
-    alert(`Invalid choice sir, therefore we just picked ${SCISSORS} for you`)
+    alert(`Invalid choice sir, therefore we just picked ${DEFAULT_USER_CHOICE} for you`)
   }
 }
 
